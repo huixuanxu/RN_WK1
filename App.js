f7ec39a694850cb9fa0f587d948604544c3af7f2
hsu <angel911024a@gmail.com>
2024-02-25 15:09:33 +0800

    Hello RN

---
+++ App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>數位三甲</Text>
+      <Text>111019004</Text>
+      <Text>許卉萱</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
